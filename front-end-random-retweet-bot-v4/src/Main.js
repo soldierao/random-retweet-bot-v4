b043194cecb27aebfc962e2ae258907a15930863
J.D. Jiménez <angelorganization@JDs-MacBook-Air.local>
2022-12-11 03:12:24 -0500

    getting the hang of vim

---
+++ front-end-random-retweet-bot-v4/src/Main.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function WhiteBox(){
+	<div className='white-box'>
+		
+	</div>	
+}
+
+
+export default function Main() {
+  return (
+	<>
+    	<WhiteBox/>	  
+        </> 
+);
+}
+
+export default App;
